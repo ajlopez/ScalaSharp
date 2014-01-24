f716f3e742f3421c2224214ce79f38e7cfbb43c9
ajlopez <webmaster@ajlopez.com>
2014-01-24 09:45:15 -0300

    Context first tests

---
+++ Src/ScalaSharp.Core.Tests/Contexts/ContextTests.cs
@@ -0,0 +1,31 @@
+﻿namespace ScalaSharp.Core.Tests.Contexts
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using ScalaSharp.Core.Contexts;
+
+    [TestClass]
+    public class ContextTests
+    {
+        [TestMethod]
+        public void GetUnknownValueAsNull()
+        {
+            Context context = new Context();
+
+            Assert.IsNull(context.GetValue("unknown"));
+        }
+
+        [TestMethod]
+        public void SetAndGetValue()
+        {
+            Context context = new Context();
+
+            context.SetValue("one", 1);
+
+            Assert.AreEqual(1, context.GetValue("one"));
+        }
+    }
+}
