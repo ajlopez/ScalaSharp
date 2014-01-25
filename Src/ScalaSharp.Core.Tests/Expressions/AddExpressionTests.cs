79e4e70bd5df202f3b3467903b16664635de87e3
ajlopez <webmaster@ajlopez.com>
2014-01-25 17:49:15 -0300

    Binary arithmetic expressions, with tests, borrowed and adapted from RubySharp

---
+++ Src/ScalaSharp.Core.Tests/Expressions/AddExpressionTests.cs
@@ -0,0 +1,61 @@
+﻿namespace ScalaSharp.Core.Tests.Expressions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using ScalaSharp.Core.Expressions;
+
+    [TestClass]
+    public class AddExpressionTests
+    {
+        [TestMethod]
+        public void AddTwoIntegers()
+        {
+            AddExpression expr = new AddExpression(new ConstantExpression(1), new ConstantExpression(2));
+
+            Assert.AreEqual(3, expr.Evaluate(null));
+        }
+
+        [TestMethod]
+        public void AddIntegerToDouble()
+        {
+            AddExpression expr = new AddExpression(new ConstantExpression(1), new ConstantExpression(2.5));
+
+            Assert.AreEqual(1 + 2.5, expr.Evaluate(null));
+        }
+
+        [TestMethod]
+        public void AddDoubleToInteger()
+        {
+            AddExpression expr = new AddExpression(new ConstantExpression(2.5), new ConstantExpression(1));
+
+            Assert.AreEqual(2.5 + 1, expr.Evaluate(null));
+        }
+
+        [TestMethod]
+        public void AddTwoDoubles()
+        {
+            AddExpression expr = new AddExpression(new ConstantExpression(2.5), new ConstantExpression(3.7));
+
+            Assert.AreEqual(2.5 + 3.7, expr.Evaluate(null));
+        }
+
+        [TestMethod]
+        public void AddStringToInteger()
+        {
+            AddExpression expr = new AddExpression(new ConstantExpression("42"), new ConstantExpression(1));
+
+            Assert.AreEqual("421", expr.Evaluate(null));
+        }
+
+        [TestMethod]
+        public void AddIntegerToString()
+        {
+            AddExpression expr = new AddExpression(new ConstantExpression(42), new ConstantExpression("1"));
+
+            Assert.AreEqual("421", expr.Evaluate(null));
+        }
+    }
+}
---
+++ Src/ScalaSharp.Core/Expressions/BinaryExpression.cs
@@ -0,0 +1,34 @@
+﻿namespace ScalaSharp.Core.Expressions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using ScalaSharp.Core.Contexts;
+
+    public abstract class BinaryExpression : IExpression
+    {
+        private IExpression left;
+        private IExpression right;
+
+        public BinaryExpression(IExpression left, IExpression right)
+        {
+            this.left = left;
+            this.right = right;
+        }
+
+        public IExpression LeftExpression { get { return this.left; } }
+
+        public IExpression RightExpression { get { return this.right; } }
+
+        public object Evaluate(IContext context)
+        {
+            var lvalue = this.left.Evaluate(context);
+            var rvalue = this.right.Evaluate(context);
+
+            return this.Apply(lvalue, rvalue);
+        }
+
+        public abstract object Apply(object leftvalue, object rightvalue);
+    }
+}
