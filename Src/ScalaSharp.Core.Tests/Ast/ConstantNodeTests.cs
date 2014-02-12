b52b1084228200987118357b21c016fe1da4fb7c
ajlopez <webmaster@ajlopez.com>
2014-02-12 14:00:00 -0300

    ConstantNode first test

---
+++ Src/ScalaSharp.Core.Tests/Ast/ConstantNodeTests.cs
@@ -0,0 +1,22 @@
+﻿namespace ScalaSharp.Core.Tests.Ast
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using ScalaSharp.Core.Ast;
+
+    [TestClass]
+    public class ConstantNodeTests
+    {
+        [TestMethod]
+        public void CreateNodeWithIntegerValue()
+        {
+            ConstantNode node = new ConstantNode(42);
+
+            Assert.IsNotNull(node.Value);
+            Assert.AreEqual(42, node.Value);
+        }
+    }
+}
---
+++ Src/ScalaSharp.Core/Ast/ConstantNode.cs
@@ -0,0 +1,19 @@
+﻿namespace ScalaSharp.Core.Ast
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class ConstantNode
+    {
+        private object value;
+
+        public ConstantNode(object value)
+        {
+            this.value = value;
+        }
+
+        public object Value { get { return this.value; } }
+    }
+}
