bf0fcb28189efcd061c6db48dda77ee7c0c19744
ajlopez <webmaster@ajlopez.com>
2014-02-12 13:52:40 -0300

    AST first node, NameNode first test

---
+++ Src/ScalaSharp.Core.Tests/Ast/NameNodeTests.cs
@@ -0,0 +1,22 @@
+﻿namespace ScalaSharp.Core.Tests.Ast
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using ScalaSharp.Core.Ast;
+
+    [TestClass]
+    public class NameNodeTests
+    {
+        [TestMethod]
+        public void CreateNodeWithName()
+        {
+            NameNode node = new NameNode("foo");
+
+            Assert.IsNotNull(node.Name);
+            Assert.AreEqual("foo", node.Name);
+        }
+    }
+}
---
+++ Src/ScalaSharp.Core/Ast/NameNode.cs
@@ -0,0 +1,19 @@
+﻿namespace ScalaSharp.Core.Ast
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class NameNode
+    {
+        private string name;
+
+        public NameNode(string name)
+        {
+            this.name = name;
+        }
+
+        public string Name { get { return this.name; } }
+    }
+}
