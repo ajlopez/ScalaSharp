9fa6c2cffd242c04a11f6a266166b87af76dafa1
ajlopez <webmaster@ajlopez.com>
2014-02-17 11:20:04 -0300

    ObjectNode first test

---
+++ Src/ScalaSharp.Core.Tests/Ast/ObjectNodeTests.cs
@@ -0,0 +1,23 @@
+﻿namespace ScalaSharp.Core.Tests.Ast
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using ScalaSharp.Core.Ast;
+
+    [TestClass]
+    public class ObjectNodeTests
+    {
+        [TestMethod]
+        public void CreateObjectNodeWithNameAndNullBody()
+        {
+            ObjectNode node = new ObjectNode("Foo", null);
+
+            Assert.AreEqual("Foo", node.Name);
+            Assert.IsNull(node.Body);
+            Assert.AreEqual("Foo", node.TypeInfo.Name);
+        }
+    }
+}
---
+++ Src/ScalaSharp.Core/Ast/ObjectNode.cs
@@ -0,0 +1,28 @@
+﻿namespace ScalaSharp.Core.Ast
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using ScalaSharp.Core.Language;
+
+    public class ObjectNode : INode
+    {
+        private string name;
+        private INode body;
+        private TypeInfo typeinfo;
+
+        public ObjectNode(string name, INode body)
+        {
+            this.name = name;
+            this.body = body;
+            this.typeinfo = TypeInfo.MakeByName(name);
+        }
+
+        public string Name { get { return this.name; } }
+
+        public INode Body { get { return this.body; } }
+
+        public TypeInfo TypeInfo { get { return this.typeinfo; } }
+    }
+}
