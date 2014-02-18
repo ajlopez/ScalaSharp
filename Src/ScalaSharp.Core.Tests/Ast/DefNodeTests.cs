a5fa7704c28aa8cf7a581d5bb1d7d1be03c7de9a
ajlopez <webmaster@ajlopez.com>
2014-02-18 07:42:33 -0300

    DefNode first test

---
+++ Src/ScalaSharp.Core.Tests/Ast/DefNodeTests.cs
@@ -0,0 +1,30 @@
+﻿namespace ScalaSharp.Core.Tests.Ast
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using ScalaSharp.Core.Ast;
+    using ScalaSharp.Core.Language;
+
+    [TestClass]
+    public class DefNodeTests
+    {
+        [TestMethod]
+        public void CreateDefNode()
+        {
+            string name = "foo";
+            IList<ArgumentInfo> arguments = new List<ArgumentInfo>() { new ArgumentInfo("a", TypeInfo.Int), new ArgumentInfo("b", TypeInfo.Double) };
+            TypeInfo typeinfo = TypeInfo.Int;
+            INode expression = new ConstantNode(42);
+
+            DefNode node = new DefNode(name, arguments, typeinfo, expression);
+
+            Assert.AreEqual(name, node.Name);
+            Assert.AreSame(arguments, node.Arguments);
+            Assert.AreSame(TypeInfo.Int, node.TypeInfo);
+            Assert.AreSame(expression, node.Expression);
+        }
+    }
+}
---
+++ Src/ScalaSharp.Core/Ast/DefNode.cs
@@ -0,0 +1,32 @@
+﻿namespace ScalaSharp.Core.Ast
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using ScalaSharp.Core.Language;
+
+    public class DefNode
+    {
+        private string name;
+        private IList<ArgumentInfo> arguments; 
+        private TypeInfo typeinfo;
+        private INode expression;
+
+        public DefNode(string name, IList<ArgumentInfo> arguments, TypeInfo typeinfo, INode expression)
+        {
+            this.name = name;
+            this.arguments = arguments;
+            this.typeinfo = typeinfo;
+            this.expression = expression;
+        }
+
+        public string Name { get { return this.name; } }
+
+        public IList<ArgumentInfo> Arguments { get { return this.arguments; } }
+
+        public TypeInfo TypeInfo { get { return this.typeinfo; } }
+
+        public INode Expression { get { return this.expression; } }
+    }
+}
