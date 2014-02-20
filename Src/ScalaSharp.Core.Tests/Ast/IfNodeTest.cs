58178cc2f2f7cf769f87171642d41d06529d13b2
ajlopez <webmaster@ajlopez.com>
2014-02-20 18:00:57 -0300

    IfNode simple creation test

---
+++ Src/ScalaSharp.Core.Tests/Ast/IfNodeTest.cs
@@ -0,0 +1,28 @@
+﻿namespace ScalaSharp.Core.Tests.Ast
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using ScalaSharp.Core.Ast;
+    using ScalaSharp.Core.Language;
+
+    [TestClass]
+    public class IfNodeTest
+    {
+        [TestMethod]
+        public void CreateIfNode()
+        {
+            var condition = new ConstantNode(true);
+            var then = new ConstantNode(42);
+
+            var node = new IfNode(condition, then, null);
+
+            Assert.AreSame(condition, node.Condition);
+            Assert.AreSame(then, node.Then);
+            Assert.IsNull(node.Else);
+            Assert.AreSame(TypeInfo.Int, node.TypeInfo);
+        }
+    }
+}
---
+++ Src/ScalaSharp.Core/Ast/IfNode.cs
@@ -0,0 +1,30 @@
+﻿namespace ScalaSharp.Core.Ast
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using ScalaSharp.Core.Language;
+
+    public class IfNode : INode
+    {
+        private INode condition;
+        private INode then;
+        private INode @else;
+
+        public IfNode(INode condition, INode then, INode @else)
+        {
+            this.condition = condition;
+            this.then = then;
+            this.@else = @else;
+        }
+
+        public INode Condition { get { return this.condition; } }
+
+        public INode Then { get { return this.then; } }
+
+        public INode Else { get { return this.@else; } }
+
+        public TypeInfo TypeInfo { get { return this.then.TypeInfo; } }
+    }
+}
