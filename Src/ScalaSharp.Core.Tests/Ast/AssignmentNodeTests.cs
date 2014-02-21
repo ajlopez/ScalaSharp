31a204191160e3cccdbfde615f17307585386d8a
ajlopez <webmaster@ajlopez.com>
2014-02-21 08:37:57 -0300

    AssignmentNode with simple creation test

---
+++ Src/ScalaSharp.Core.Tests/Ast/AssignmentNodeTests.cs
@@ -0,0 +1,27 @@
+﻿namespace ScalaSharp.Core.Tests.Ast
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using ScalaSharp.Core.Ast;
+    using ScalaSharp.Core.Language;
+
+    [TestClass]
+    public class AssignmentNodeTests
+    {
+        [TestMethod]
+        public void CreateAssignmentNode()
+        {
+            NameNode target = new NameNode("foo");
+            ConstantNode expr = new ConstantNode(42);
+            AssignmentNode node = new AssignmentNode(target, expr);
+
+            Assert.AreSame(target, node.Target);
+            Assert.AreSame(expr, node.Expression);
+            Assert.AreSame(TypeInfo.Int, node.TypeInfo);
+        }
+    }
+}
+
---
+++ Src/ScalaSharp.Core/Ast/AssignmentNode.cs
@@ -0,0 +1,26 @@
+﻿namespace ScalaSharp.Core.Ast
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+using ScalaSharp.Core.Language;
+
+    public class AssignmentNode : INode
+    {
+        private INode target;
+        private INode expression;
+
+        public AssignmentNode(INode target, INode expression)
+        {
+            this.target = target;
+            this.expression = expression;
+        }
+
+        public INode Target { get { return this.target; } }
+
+        public INode Expression { get { return this.expression; } }
+
+        public TypeInfo TypeInfo { get { return this.expression.TypeInfo; } }
+    }
+}
