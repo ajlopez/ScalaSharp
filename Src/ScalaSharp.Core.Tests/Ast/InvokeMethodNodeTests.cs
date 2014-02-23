e433fab24b46e61a71bff862d414b02cc9d95f3c
ajlopez <webmaster@ajlopez.com>
2014-02-23 15:18:57 -0300

    InvokeMethodNode with simple creation test

---
+++ Src/ScalaSharp.Core.Tests/Ast/InvokeMethodNodeTests.cs
@@ -0,0 +1,25 @@
+﻿namespace ScalaSharp.Core.Tests.Ast
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using ScalaSharp.Core.Ast;
+
+    [TestClass]
+    public class InvokeMethodNodeTests
+    {
+        [TestMethod]
+        public void CreateInvokeMethodNode()
+        {
+            INode target = new NameNode("foo");
+            IList<INode> arguments = new List<INode>() { new ConstantNode(42) };
+            InvokeMethodNode node = new InvokeMethodNode(target, "append", arguments);
+
+            Assert.AreSame(target, node.Target);
+            Assert.AreSame(arguments, node.Arguments);
+            Assert.AreEqual("append", node.MethodName);
+        }
+    }
+}
---
+++ Src/ScalaSharp.Core/Ast/InvokeMethodNode.cs
@@ -0,0 +1,27 @@
+﻿namespace ScalaSharp.Core.Ast
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class InvokeMethodNode
+    {
+        private INode target;
+        private string methodname;
+        private IList<INode> arguments;
+
+        public InvokeMethodNode(INode target, string methodname, IList<INode> arguments)
+        {
+            this.target = target;
+            this.methodname = methodname;
+            this.arguments = arguments;
+        }
+
+        public INode Target { get { return this.target; } }
+
+        public string MethodName { get { return this.methodname; } }
+
+        public IList<INode> Arguments { get { return this.arguments; } }
+    }
+}
