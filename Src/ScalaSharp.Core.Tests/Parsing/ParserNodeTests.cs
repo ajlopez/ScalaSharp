a9024b7db4b1007043cadae5644cb53bda008ace
ajlopez <webmaster@ajlopez.com>
2014-03-18 18:42:21 -0300

    Additional tests, parse val node

--- Src/ScalaSharp.Core.Tests/Parsing/ParserNodeTests.cs
+++ Src/ScalaSharp.Core.Tests/Parsing/ParserNodeTests.cs
@@ -315,6 +315,52 @@
         }
 
         [TestMethod]
+        public void ParseSimpleValNodeWithEndOfLine()
+        {
+            Parser parser = new Parser("val one = 1\n");
+
+            var node = parser.ParseNode();
+
+            Assert.IsNotNull(node);
+            Assert.IsInstanceOfType(node, typeof(ValNode));
+
+            var vnode = (ValNode)node;
+
+            Assert.AreEqual("one", vnode.Name);
+            Assert.IsNotNull(vnode.Expression);
+            Assert.IsInstanceOfType(vnode.Expression, typeof(ConstantNode));
+
+            var expr = (ConstantNode)vnode.Expression;
+
+            Assert.AreEqual(1, expr.Value);
+
+            Assert.IsNull(parser.ParseNode());
+        }
+
+        [TestMethod]
+        public void ParseSimpleValNodeWithSemicolon()
+        {
+            Parser parser = new Parser("val one = 1;");
+
+            var node = parser.ParseNode();
+
+            Assert.IsNotNull(node);
+            Assert.IsInstanceOfType(node, typeof(ValNode));
+
+            var vnode = (ValNode)node;
+
+            Assert.AreEqual("one", vnode.Name);
+            Assert.IsNotNull(vnode.Expression);
+            Assert.IsInstanceOfType(vnode.Expression, typeof(ConstantNode));
+
+            var expr = (ConstantNode)vnode.Expression;
+
+            Assert.AreEqual(1, expr.Value);
+
+            Assert.IsNull(parser.ParseNode());
+        }
+
+        [TestMethod]
         public void RaiseIfNotImplemented()
         {
             Parser parser = new Parser("[]");
