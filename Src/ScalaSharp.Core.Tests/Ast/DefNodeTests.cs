670a4a4f225a28fd499a878ed48911ad6a8dc77c
ajlopez <webmaster@ajlopez.com>
2014-05-31 17:27:08 -0300

    DefNode as ICommandNode

--- Src/ScalaSharp.Core.Tests/Ast/DefNodeTests.cs
+++ Src/ScalaSharp.Core.Tests/Ast/DefNodeTests.cs
@@ -6,6 +6,8 @@
     using System.Text;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using ScalaSharp.Core.Ast;
+    using ScalaSharp.Core.Commands;
+    using ScalaSharp.Core.Expressions;
     using ScalaSharp.Core.Language;
 
     [TestClass]
@@ -42,6 +44,37 @@
             node.CheckType();
 
             Assert.AreSame(TypeInfo.Int, node.TypeInfo);
+        }
+
+        [TestMethod]
+        public void GetCommand()
+        {
+            string name = "foo";
+            IList<ArgumentInfo> arguments = new List<ArgumentInfo>() { new ArgumentInfo("a", TypeInfo.Int), new ArgumentInfo("b", TypeInfo.Double) };
+            TypeInfo typeinfo = TypeInfo.Int;
+            IExpressionNode expression = new ConstantNode(42);
+
+            DefNode node = new DefNode(name, arguments, typeinfo, expression);
+
+            var result = node.GetCommand();
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(DefCommand));
+
+            var dcmd = (DefCommand)result;
+
+            Assert.AreEqual(name, dcmd.Name);
+            Assert.AreEqual(TypeInfo.Int, dcmd.TypeInfo);
+            Assert.IsNotNull(dcmd.Arguments);
+            Assert.AreEqual(2, dcmd.Arguments.Count);
+            Assert.AreSame(arguments, dcmd.Arguments);
+
+            Assert.IsNotNull(dcmd.Body);
+            Assert.IsInstanceOfType(dcmd.Body, typeof(ExpressionCommand));
+
+            var body = (ExpressionCommand)dcmd.Body;
+            Assert.IsInstanceOfType(body.Expression, typeof(ConstantExpression));
+            Assert.AreEqual(42, ((ConstantExpression)body.Expression).Value);
         }
 
         [TestMethod]
--- Src/ScalaSharp.Core/Ast/DefNode.cs
+++ Src/ScalaSharp.Core/Ast/DefNode.cs
@@ -4,9 +4,10 @@
     using System.Collections.Generic;
     using System.Linq;
     using System.Text;
+    using ScalaSharp.Core.Commands;
     using ScalaSharp.Core.Language;
 
-    public class DefNode : NamedExpressionNode
+    public class DefNode : NamedExpressionNode, ICommandNode
     {
         private IList<ArgumentInfo> arguments; 
 
@@ -17,5 +18,11 @@
         }
 
         public IList<ArgumentInfo> Arguments { get { return this.arguments; } }
+
+        public ICommand GetCommand()
+        {
+            return new DefCommand(this.Name, this.arguments, this.TypeInfo, new ExpressionCommand(this.Expression.GetExpression()));
+            throw new NotImplementedException();
+        }
     }
 }
