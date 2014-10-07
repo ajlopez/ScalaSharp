b2a7248e7357997d68112c577ab19a9f48b896a8
ajlopez <webmaster@ajlopez.com>
2014-10-07 08:22:25 -0300

    ModulusExpression first tests

---
+++ Src/ScalaSharp.Core.Tests/Expressions/ModulusExpressionTests.cs
@@ -0,0 +1,71 @@
+﻿namespace ScalaSharp.Core.Tests.Expressions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using ScalaSharp.Core.Expressions;
+    using ScalaSharp.Core.Language;
+
+    [TestClass]
+    public class ModulusExpressionTests
+    {
+        [TestMethod]
+        public void ModulusTwoIntegers()
+        {
+            ModulusExpression expr = new ModulusExpression(new ConstantExpression(6), new ConstantExpression(2));
+
+            Assert.AreEqual(0, expr.Evaluate(null));
+            Assert.AreSame(TypeInfo.Int, expr.TypeInfo);
+        }
+
+        [TestMethod]
+        public void ModulusIntegerByDouble()
+        {
+            ModulusExpression expr = new ModulusExpression(new ConstantExpression(2), new ConstantExpression(2.5));
+
+            try
+            {
+                expr.Evaluate(null);
+                Assert.Fail();
+            }
+            catch (Exception ex)
+            {
+                Assert.IsInstanceOfType(ex, typeof(InvalidOperationException));
+            }
+        }
+
+        [TestMethod]
+        public void ModulusDoubleByInteger()
+        {
+            ModulusExpression expr = new ModulusExpression(new ConstantExpression(2.5), new ConstantExpression(3));
+
+            try
+            {
+                expr.Evaluate(null);
+                Assert.Fail();
+            }
+            catch (Exception ex)
+            {
+                Assert.IsInstanceOfType(ex, typeof(InvalidOperationException));
+            }
+        }
+
+        [TestMethod]
+        public void ModulusTwoDoubles()
+        {
+            ModulusExpression expr = new ModulusExpression(new ConstantExpression(2.5), new ConstantExpression(3.7));
+
+            try
+            {
+                expr.Evaluate(null);
+                Assert.Fail();
+            }
+            catch (Exception ex)
+            {
+                Assert.IsInstanceOfType(ex, typeof(InvalidOperationException));
+            }
+        }
+    }
+}
---
+++ Src/ScalaSharp.Core/Expressions/ModulusExpression.cs
@@ -0,0 +1,24 @@
+﻿namespace ScalaSharp.Core.Expressions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class ModulusExpression : BinaryArithmeticExpression
+    {
+        public ModulusExpression(IExpression left, IExpression right)
+            : base(left, right)
+        {
+        }
+
+        public override object Apply(object leftvalue, object rightvalue)
+        {
+            if (leftvalue is int)
+                if (rightvalue is int)
+                    return (int)leftvalue % (int)rightvalue;
+
+            throw new InvalidOperationException();
+        }
+    }
+}
